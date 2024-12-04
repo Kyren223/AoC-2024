f60bad93e38c627a4cc913e3c6d2e6e2d8bc1833
Kyren223 <Kyren223@proton.me>
2024-12-04 12:02:40 +0200

    Day 4 Part 1 Done, took a shit ton of time bcz I had a bunch of issues with zig so I had to figure stuff out and that took most of the time, I think I solved the problem in like ~10m after I got everything to work.

--- day4/main.zig
+++ day4/main.zig
@@ -1,5 +1,104 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello, World!\n", .{});
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    var bufReader = std.io.bufferedReader(file.reader());
+    var inStream = bufReader.reader();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const ally = arena.allocator();
+
+    var lines = std.ArrayList([]u8).init(ally);
+
+    var buf: [1024]u8 = undefined;
+    while (try inStream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const copyLine = try ally.alloc(u8, line.len);
+        std.mem.copyForwards(u8, copyLine, line);
+        try lines.append(copyLine);
+    }
+
+    for (lines.items) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
+
+    const length = 3;
+
+    var count: u32 = 0;
+    count += 0;
+    for (lines.items, 0..) |line, uy| {
+        lineFor: for (line, 0..) |c, ux| {
+            if (c != 'X') continue :lineFor;
+            const y: isize = @intCast(uy);
+            const x: isize = @intCast(ux);
+            if (x + length < line.len) {
+                const m = line[ux + 1] == 'M';
+                const a = line[ux + 2] == 'A';
+                const s = line[ux + 3] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (0 <= x - length) {
+                const m = line[ux - 1] == 'M';
+                const a = line[ux - 2] == 'A';
+                const s = line[ux - 3] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (0 <= y - length) {
+                const m = lines.items[uy - 1][ux] == 'M';
+                const a = lines.items[uy - 2][ux] == 'A';
+                const s = lines.items[uy - 3][ux] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (y + length < lines.items.len) {
+                const m = lines.items[uy + 1][ux] == 'M';
+                const a = lines.items[uy + 2][ux] == 'A';
+                const s = lines.items[uy + 3][ux] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (y + length < lines.items.len and x + length < line.len) {
+                const m = lines.items[uy + 1][ux + 1] == 'M';
+                const a = lines.items[uy + 2][ux + 2] == 'A';
+                const s = lines.items[uy + 3][ux + 3] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (0 <= y - length and x + length < line.len) {
+                const m = lines.items[uy - 1][ux + 1] == 'M';
+                const a = lines.items[uy - 2][ux + 2] == 'A';
+                const s = lines.items[uy - 3][ux + 3] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (0 <= y - length and 0 <= x - length) {
+                const m = lines.items[uy - 1][ux - 1] == 'M';
+                const a = lines.items[uy - 2][ux - 2] == 'A';
+                const s = lines.items[uy - 3][ux - 3] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+            if (y + length < lines.items.len and 0 <= x - length) {
+                const m = lines.items[uy + 1][ux - 1] == 'M';
+                const a = lines.items[uy + 2][ux - 2] == 'A';
+                const s = lines.items[uy + 3][ux - 3] == 'S';
+                if (m and a and s) {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    std.debug.print("Example: {}\n", .{count});
 }
