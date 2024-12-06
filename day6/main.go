9099693d14cf61d5f5441573be895c53b5f3c25d
Kyren223 <Kyren223@proton.me>
2024-12-06 10:46:28 +0200

    Day 6 Start

---
+++ day6/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.ReadFile("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input := string(file)
+	fmt.Println("Part 1 Example:", Part1(input))
+
+	// file, err = os.ReadFile("input.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// input = string(file)
+	// fmt.Println("Part 1:", Part1(input))
+	//
+	// file, err = os.ReadFile("example.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// input = string(file)
+	// fmt.Println("Part 2 Example:", Part2(input))
+	//
+	// file, err = os.ReadFile("input.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// input = string(file)
+	// fmt.Println("Part 2:", Part2(input))
+}
+
+func Part1(input string) int {
+	return 0
+}
+
+func Part2(input string) int {
+	return 0
+}
