25729880cb464bfbd9508345e829f6116b528a8d
Kyren223 <Kyren223@proton.me>
2024-12-08 10:52:07 +0200

    Day 8 Part 2 Done

--- day8/main.go
+++ day8/main.go
@@ -22,19 +22,19 @@ func main() {
 	input = string(file)
 	fmt.Println("Part 1:", Part1(input))
 
-	// file, err = os.ReadFile("example.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 2 Example:", Part2(input))
-	//
-	// file, err = os.ReadFile("input.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 2:", Part2(input))
+	file, err = os.ReadFile("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 2 Example:", Part2(input))
+
+	file, err = os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 2:", Part2(input))
 }
 
 func Part1(input string) int {
@@ -55,7 +55,7 @@ func Part1(input string) int {
 			if ('0' > c || c > '9') && ('a' > c || c > 'z') && ('A' > c || c > 'Z') {
 				continue
 			}
-			fmt.Println(x, y, c)
+			// fmt.Println(x, y, c)
 
 			for y2, line2 := range antenasMap {
 				for x2, c2 := range line2 {
@@ -89,6 +89,73 @@ func Part1(input string) int {
 		for _, marked := range mark {
 			if marked {
 				sum++
+				// fmt.Print("#")
+			} else {
+				// fmt.Print(".")
+			}
+		}
+		// fmt.Print("\n")
+	}
+
+	return sum
+}
+
+func Part2(input string) int {
+	antenasMap := strings.Split(input, "\n")
+	antenasMap = antenasMap[:len(antenasMap)-1]
+
+	// fmt.Println("Width:", len(antenasMap[0]), "Height:", len(antenasMap))
+	width := len(antenasMap[0])
+	height := len(antenasMap)
+
+	marks := make([][]bool, height)
+	for y := range marks {
+		marks[y] = make([]bool, width)
+	}
+
+	for y, line := range antenasMap {
+		for x, c := range line {
+			if ('0' > c || c > '9') && ('a' > c || c > 'z') && ('A' > c || c > 'Z') {
+				continue
+			}
+			// fmt.Println(x, y, c)
+
+			for y2, line2 := range antenasMap {
+				for x2, c2 := range line2 {
+					if c != c2 || (x == x2 && y == y2) {
+						continue
+					}
+					marks[y][x] = true
+
+					xDiff := x2 - x
+					yDiff := y2 - y
+
+					xPoint1 := x2 + xDiff
+					yPoint1 := y2 + yDiff
+					for 0 <= xPoint1 && xPoint1 < width && 0 <= yPoint1 && yPoint1 < height {
+						marks[yPoint1][xPoint1] = true
+						xPoint1 = xPoint1 + xDiff
+						yPoint1 = yPoint1 + yDiff
+					}
+
+					xPoint2 := x - xDiff
+					yPoint2 := y - yDiff
+					for 0 <= xPoint2 && xPoint2 < width && 0 <= yPoint2 && yPoint2 < height {
+						marks[yPoint2][xPoint2] = true
+						xPoint2 = xPoint2 - xDiff
+						yPoint2 = yPoint2 - yDiff
+					}
+				}
+			}
+
+		}
+	}
+
+	sum := 0
+	for _, mark := range marks {
+		for _, marked := range mark {
+			if marked {
+				sum++
 				fmt.Print("#")
 			} else {
 				fmt.Print(".")
@@ -97,10 +164,5 @@ func Part1(input string) int {
 		fmt.Print("\n")
 	}
 
-	return sum
-}
-
-func Part2(input string) int {
-	sum := 0
 	return sum
 }
