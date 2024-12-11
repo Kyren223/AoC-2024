fba188bf8d2025f594f27a513c5e78d0d6ff90cc
Kyren223 <Kyren223@proton.me>
2024-12-11 08:29:15 +0200

    Day 11 Part 2 Done (I did get a hint by looking at the subreddit and I read "python solutin using recursion amd memoization" (without looking at the solution obv), and then realized I need to memoize it, I did think of memoizing it but I thought it wouldn't matter and will just take more space which will make it slower, but bcz of that I did try it and it was what was needed)

--- day11/main.go
+++ day11/main.go
@@ -9,19 +9,19 @@ import (
 )
 
 func main() {
-	file, err := os.ReadFile("example.txt")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	input := string(file)
-	fmt.Println("Part 1 Example:", Part1(input))
+	// file, err := os.ReadFile("example.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// input := string(file)
+	// fmt.Println("Part 1 Example:", Part1(input))
 
-	file, err = os.ReadFile("input.txt")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	input = string(file)
-	fmt.Println("Part 1:", Part1(input))
+	// file, err := os.ReadFile("input.txt")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// input := string(file)
+	// fmt.Println("Part 1:", Part1(input))
 
 	// file, err = os.ReadFile("example.txt")
 	// if err != nil {
@@ -29,13 +29,13 @@ func main() {
 	// }
 	// input = string(file)
 	// fmt.Println("Part 2 Example:", Part2(input))
-	//
-	// file, err = os.ReadFile("input.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 2:", Part2(input))
+
+	file, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input := string(file)
+	fmt.Println("Part 2:", Part2(input))
 }
 
 func Part1(input string) int {
@@ -48,12 +48,25 @@ func Part1(input string) int {
 		}
 		stones = append(stones, int(stone))
 	}
+	fmt.Println(stones)
 
-	for i := 0; i < 25; i++ {
+	// sum := 0
+	// for j := 0; j < len(stones); j++ {
+	// 	tmpStones := stones[j : j+1]
+	// 	for i := 0; i < 75; i++ {
+	// 		// fmt.Println(stones)
+	// 		tmpStones = Blink(tmpStones)
+	// 	}
+	// 	// fmt.Println(stones)
+	// 	sum += len(tmpStones)
+	// }
+
+	stones = stones[0:1]
+	for i := 0; i < 75; i++ {
 		// fmt.Println(stones)
 		stones = Blink(stones)
 	}
-	// fmt.Println(stones)
+	fmt.Println(stones)
 
 	return len(stones)
 }
@@ -62,24 +75,80 @@ func Blink(stones []int) []int {
 	var modified []int
 
 	for _, num := range stones {
-		s := strconv.FormatInt(int64(num), 10)
 		if num == 0 {
 			modified = append(modified, 1)
-		} else if len(s)%2 == 0 {
-			middle := len(s) / 2
-			before, _ := strconv.ParseInt(s[:middle], 10, 64)
-			after, _ := strconv.ParseInt(s[middle:], 10, 64)
-			modified = append(modified, int(before))
-			modified = append(modified, int(after))
 		} else {
-			modified = append(modified, num*2024)
+			s := strconv.FormatInt(int64(num), 10)
+			if len(s)%2 == 0 {
+				middle := len(s) / 2
+				before, _ := strconv.ParseInt(s[:middle], 10, 64)
+				after, _ := strconv.ParseInt(s[middle:], 10, 64)
+				modified = append(modified, int(before))
+				modified = append(modified, int(after))
+			} else {
+				modified = append(modified, num*2024)
+			}
 		}
 	}
 
 	return modified
 }
 
+type Point struct {
+	stone int64
+	depth int
+}
+
+var memo = map[Point]int64{}
+
+func Blink2(stone int64, depth int) int64 {
+	mem, ok := memo[Point{stone, depth}]
+	if ok {
+		return mem
+	}
+
+	if depth == 75 {
+		return 1
+	}
+
+	if stone == 0 {
+		result := Blink2(1, depth+1)
+		memo[Point{stone, depth}] = result
+		return result
+	}
+
+	s := strconv.FormatInt(int64(stone), 10)
+	if len(s)%2 == 0 {
+		middle := len(s) / 2
+		before, _ := strconv.ParseInt(s[:middle], 10, 64)
+		after, _ := strconv.ParseInt(s[middle:], 10, 64)
+
+		result := Blink2(before, depth+1) + Blink2(after, depth+1)
+		memo[Point{stone, depth}] = result
+		return result
+	}
+
+	result := Blink2(stone*2024, depth+1)
+	memo[Point{stone, depth}] = result
+	return result
+}
+
 func Part2(input string) int {
-	sum := 0
-	return sum
+	var stones []int64
+
+	for _, stoneStr := range strings.Split(input, " ") {
+		stone, err := strconv.ParseInt(stoneStr, 10, 64)
+		if err != nil {
+			continue
+		}
+		stones = append(stones, stone)
+	}
+	fmt.Println(stones)
+
+	sum := int64(0)
+	for i := 0; i < len(stones); i++ {
+		sum += Blink2(stones[i], 0)
+	}
+
+	return int(sum)
 }
