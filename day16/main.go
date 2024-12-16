2348cc7b15f44652c76795089af1e4a0ab8cfe45
Kyren223 <Kyren223@proton.me>
2024-12-16 11:38:54 +0200

    Day 16 Part 2 Failed (will work on it later, probably in a few days)

--- day16/main.go
+++ day16/main.go
@@ -15,20 +15,20 @@ func main() {
 	input := string(file)
 	fmt.Println("Part 1 Example:", Part1(input))
 
-	file, err = os.ReadFile("input.txt")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	input = string(file)
-	fmt.Println("Part 1:", Part1(input))
-
-	// file, err = os.ReadFile("example.txt")
+	// file, err = os.ReadFile("input.txt")
 	// if err != nil {
 	// 	log.Fatalln(err)
 	// }
 	// input = string(file)
-	// fmt.Println("Part 2 Example:", Part2(input))
-	//
+	// fmt.Println("Part 1:", Part1(input))
+
+	file, err = os.ReadFile("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 2 Example:", Part2(input))
+
 	// file, err = os.ReadFile("input.txt")
 	// if err != nil {
 	// 	log.Fatalln(err)
@@ -65,8 +65,8 @@ func Part1(input string) int {
 	}
 
 	DFS(maze, visisted, end, 0, 1, 1, 0)
-	fmt.Println(start, end)
-	fmt.Println(visisted)
+	// fmt.Println(start, end)
+	// fmt.Println(visisted)
 
 	// Manually add +1000 cuz it needs to rotate once to be "north"
 	sum := visisted[start] + 1000
@@ -82,17 +82,12 @@ func DFS(maze []string, visited map[Vec2]int, pos Vec2, cost, xcost, ycost int, 
 		return
 	}
 
-	if pos.x == 13 && pos.y == 13 {
-		fmt.Println("13YO")
-	}
-
 	if visistedCost, ok := visited[pos]; ok && visistedCost <= cost {
 		return
 	}
 
 	visited[pos] = cost
 	if c == 'S' {
-
 	}
 
 	left := pos
@@ -111,6 +106,189 @@ func DFS(maze []string, visited map[Vec2]int, pos Vec2, cost, xcost, ycost int, 
 }
 
 func Part2(input string) int {
-	sum := 0
+	maze := strings.Split(input, "\n")
+	maze = maze[:len(maze)-1]
+
+	visisted := map[Vec2]int{}
+	start := Vec2{}
+	end := Vec2{}
+	for y, line := range maze {
+		for x, c := range line {
+			switch c {
+			case 'E':
+				end = Vec2{x, y}
+			case 'S':
+				start = Vec2{x, y}
+			}
+		}
+	}
+
+	DFS(maze, visisted, end, 0, 1, 1, 0)
+	// fmt.Println(start, end)
+	// fmt.Println(visisted)
+
+	// Manually add +1000 cuz it needs to rotate once to be "north"
+	sum := visisted[start] + 1000
+
+	marked := map[Vec2]bool{}
+	DFS2(maze, visisted, marked, start, sum, 0)
+
+	for y, line := range maze {
+		for x, c := range line {
+			if mark, ok := marked[Vec2{x, y}]; ok && mark {
+				fmt.Print(string('O'))
+			} else {
+				fmt.Print(string(c))
+			}
+			// if _, ok := visisted[Vec2{x, y}]; ok {
+			// 	fmt.Print(string('O'))
+			// } else {
+			// 	fmt.Print(string(c))
+			// }
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("HMM", visisted[Vec2{1, 10}], visisted[Vec2{2, 11}])
+
+	sum = 0
+	for _, mark := range marked {
+		if mark {
+			sum++
+		}
+	}
+
 	return sum
 }
+
+func DFS2(maze []string, visited map[Vec2]int, marked map[Vec2]bool, pos Vec2, minimum int, depth int) {
+	// if depth > 3 {
+	// 	return
+	// }
+	if _, ok := marked[pos]; ok {
+		return
+	}
+
+	cost, ok := visited[pos]
+	if !ok {
+		return
+	}
+
+	marked[pos] = cost == minimum
+	if cost > minimum {
+		// panic("wrong!")
+		return
+	}
+
+	left := pos
+	left.x -= 1
+	right := pos
+	right.x += 1
+	top := pos
+	top.y -= 1
+	bottom := pos
+	bottom.y += 1
+
+	newMin := minimum
+	if cost, ok := visited[left]; ok && cost < minimum {
+		if newMin == minimum {
+			newMin = cost
+		}
+		newMin = min(newMin, cost)
+	}
+	if cost, ok := visited[right]; ok && cost < minimum {
+		if newMin == minimum {
+			newMin = cost
+		}
+		newMin = min(newMin, cost)
+		if depth == 3 {
+			fmt.Println("RIGHT", newMin, cost, pos)
+		}
+	}
+	if cost, ok := visited[top]; ok && cost < minimum {
+		if newMin == minimum {
+			newMin = cost
+		}
+		newMin = min(newMin, cost)
+		if depth == 3 {
+			fmt.Println("TOP", newMin, cost)
+		}
+	}
+	if cost, ok := visited[bottom]; ok && cost < minimum {
+		if newMin == minimum {
+			newMin = cost
+		}
+		newMin = min(newMin, cost)
+	}
+
+	if newMin == minimum {
+		fmt.Println("Test")
+		return // Dead end
+	}
+
+	DFS2(maze, visited, marked, left, newMin, depth+1)
+	DFS2(maze, visited, marked, right, newMin, depth+1)
+	DFS2(maze, visited, marked, top, newMin, depth+1)
+	DFS2(maze, visited, marked, bottom, newMin, depth+1)
+}
+
+// func DFS2(maze []string, visited map[Vec2]int, visited2 map[Vec2]struct{}, mins map[int]int, pos Vec2, depth int) {
+// 	if _, ok := visited2[pos]; ok {
+// 		return
+// 	}
+// 	visited2[pos] = struct{}{}
+//
+// 	if m, ok := mins[depth]; ok {
+// 		mins[depth] = min(m, visited[pos])
+// 	} else {
+// 		mins[depth] = visited[pos]
+// 	}
+//
+// 	left := pos
+// 	left.x -= 1
+// 	right := pos
+// 	right.x += 1
+// 	top := pos
+// 	top.y -= 1
+// 	bottom := pos
+// 	bottom.y += 1
+//
+// 	DFS2(maze, visited, marked, left, newMin, depth+1)
+// 	DFS2(maze, visited, marked, right, newMin, depth+1)
+// 	DFS2(maze, visited, marked, top, newMin, depth+1)
+// 	DFS2(maze, visited, marked, bottom, newMin, depth+1)
+// }
+//
+// func DFS3(maze []string, visited map[Vec2]int, marked map[Vec2]bool, pos Vec2, depth int) {
+// 	// if depth > 3 {
+// 	// 	return
+// 	// }
+// 	if _, ok := marked[pos]; ok {
+// 		return
+// 	}
+//
+// 	cost, ok := visited[pos]
+// 	if !ok {
+// 		return
+// 	}
+//
+// 	marked[pos] = cost == minimum
+// 	if cost > minimum {
+// 		// panic("wrong!")
+// 		return
+// 	}
+//
+// 	left := pos
+// 	left.x -= 1
+// 	right := pos
+// 	right.x += 1
+// 	top := pos
+// 	top.y -= 1
+// 	bottom := pos
+// 	bottom.y += 1
+//
+// 	DFS2(maze, visited, marked, left, newMin, depth+1)
+// 	DFS2(maze, visited, marked, right, newMin, depth+1)
+// 	DFS2(maze, visited, marked, top, newMin, depth+1)
+// 	DFS2(maze, visited, marked, bottom, newMin, depth+1)
+// }
