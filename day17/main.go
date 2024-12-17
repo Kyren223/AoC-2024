1aa8027433ed0220265d6221ae957f43dd3e9276
Kyren223 <Kyren223@proton.me>
2024-12-17 08:15:15 +0200

    Day 17 Part 1 Done

--- day17/main.go
+++ day17/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,13 +17,13 @@ func main() {
 	input := string(file)
 	fmt.Println("Part 1 Example:", Part1(input))
 
-	// file, err = os.ReadFile("input.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 1:", Part1(input))
-	//
+	file, err = os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 1:", Part1(input))
+
 	// file, err = os.ReadFile("example.txt")
 	// if err != nil {
 	// 	log.Fatalln(err)
@@ -37,8 +40,99 @@ func main() {
 }
 
 func Part1(input string) int {
+	lines := strings.Split(input, "\n")
+	a64, _ := strconv.ParseInt(strings.TrimSpace(strings.Split(lines[0], ":")[1]), 10, 64)
+	b64, _ := strconv.ParseInt(strings.TrimSpace(strings.Split(lines[1], ":")[1]), 10, 64)
+	c64, _ := strconv.ParseInt(strings.TrimSpace(strings.Split(lines[2], ":")[1]), 10, 64)
+
+	a := int(a64)
+	b := int(b64)
+	c := int(c64)
+
+	var code []byte
+
+	programStr := strings.Split(lines[4], " ")[1]
+	bytes := strings.Split(programStr, ",")
+	for _, bitStr := range bytes {
+		bit64, _ := strconv.ParseInt(bitStr, 10, 64)
+		bit := byte(bit64)
+		code = append(code, bit)
+	}
+	fmt.Println(a, b, c, code)
+
+	out := VM(code, a, b, c)
+
+	for i, num := range out {
+		if i != 0 {
+			fmt.Print(",")
+		}
+		fmt.Print(num)
+	}
+	fmt.Println()
+
 	sum := 0
 	return sum
+}
+
+const (
+	adv = iota
+	bxl
+	bst
+	jnz
+	bxc
+	out
+	bdv
+	cdv
+)
+
+func VM(code []byte, a, b, c int) []int {
+	ip := 0
+	output := []int{}
+	for 0 <= ip && ip < len(code)-1 {
+		opcode := code[ip]
+		operand := code[ip+1]
+		// _ = opcode
+		// _ = operand
+		// fmt.Println(opcode, operand)
+		switch opcode {
+		case adv:
+			a = a / int(math.Pow(2, float64(Combo(operand, a, b, c))))
+		case bxl:
+			b ^= int(operand)
+		case bst:
+			b = Combo(operand, a, b, c) % 8
+		case jnz:
+			if a != 0 {
+				ip = int(operand) - 2
+			}
+		case bxc:
+			b ^= c
+		case out:
+			output = append(output, Combo(operand, a, b, c)%8)
+		case bdv:
+			b = a / int(math.Pow(2, float64(Combo(operand, a, b, c))))
+		case cdv:
+			c = a / int(math.Pow(2, float64(Combo(operand, a, b, c))))
+		}
+		ip += 2
+	}
+	return output
+}
+
+func Combo(operand byte, a, b, c int) int {
+	if operand <= 3 {
+		return int(operand)
+	}
+	if operand == 4 {
+		return a
+	}
+	if operand == 5 {
+		return b
+	}
+	if operand == 6 {
+		return c
+	}
+	panic("invalid")
 }
 
 func Part2(input string) int {
