72edca03436017681b4eb984231482610af995ff
Kyren223 <Kyren223@proton.me>
2024-12-19 12:20:05 +0200

    Day 19 Part 1 Done

--- day19/main.go
+++ day19/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,13 +15,13 @@ func main() {
 	input := string(file)
 	fmt.Println("Part 1 Example:", Part1(input))
 
-	// file, err = os.ReadFile("input.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 1:", Part1(input))
-	//
+	file, err = os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 1:", Part1(input))
+
 	// file, err = os.ReadFile("example.txt")
 	// if err != nil {
 	// 	log.Fatalln(err)
@@ -37,8 +38,39 @@ func main() {
 }
 
 func Part1(input string) int {
+	towels := strings.Split(strings.Split(input, "\n\n")[0], ", ")
+	designs := strings.Split(strings.Split(input, "\n\n")[1], "\n")
+	designs = designs[:len(designs)-1]
+
+	fmt.Println(towels)
+	fmt.Println(designs)
+
 	sum := 0
+	for _, design := range designs {
+		if IsPossible(towels, design) {
+			sum += 1
+		}
+	}
+
 	return sum
+}
+
+func IsPossible(towels []string, design string) bool {
+	if design == "" {
+		return true
+	}
+
+	for _, towel := range towels {
+		if !strings.HasPrefix(design, towel) {
+			continue
+		}
+		remainingDesign := design[len(towel):]
+		if IsPossible(towels, remainingDesign) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func Part2(input string) int {
