437c006e368da3a76ab787d0b27f1442c88ef0e0
Kyren223 <Kyren223@proton.me>
2024-12-19 12:33:48 +0200

    Day 19 Part 2 Done (had a typo that costed me like 5m lol)

--- day19/main.go
+++ day19/main.go
@@ -22,19 +22,19 @@ func main() {
 	input = string(file)
 	fmt.Println("Part 1:", Part1(input))
 
-	// file, err = os.ReadFile("example.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 2 Example:", Part2(input))
-	//
-	// file, err = os.ReadFile("input.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 2:", Part2(input))
+	file, err = os.ReadFile("example.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 2 Example:", Part2(input))
+
+	file, err = os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 2:", Part2(input))
 }
 
 func Part1(input string) int {
@@ -42,8 +42,8 @@ func Part1(input string) int {
 	designs := strings.Split(strings.Split(input, "\n\n")[1], "\n")
 	designs = designs[:len(designs)-1]
 
-	fmt.Println(towels)
-	fmt.Println(designs)
+	// fmt.Println(towels)
+	// fmt.Println(designs)
 
 	sum := 0
 	for _, design := range designs {
@@ -74,6 +74,41 @@ func IsPossible(towels []string, design string) bool {
 }
 
 func Part2(input string) int {
+	towels := strings.Split(strings.Split(input, "\n\n")[0], ", ")
+	designs := strings.Split(strings.Split(input, "\n\n")[1], "\n")
+	designs = designs[:len(designs)-1]
+
+	// fmt.Println(towels)
+	// fmt.Println(designs)
+
+	memo := map[string]int{}
 	sum := 0
+	for _, design := range designs {
+		sum += IsPossible2(towels, design, memo)
+	}
+
+	return sum
+}
+
+func IsPossible2(towels []string, design string, memo map[string]int) int {
+	if design == "" {
+		return 1
+	}
+
+	sum := 0
+	for _, towel := range towels {
+		if !strings.HasPrefix(design, towel) {
+			continue
+		}
+		remainingDesign := design[len(towel):]
+		if value, ok := memo[remainingDesign]; ok {
+			sum += value
+		} else {
+			value := IsPossible2(towels, remainingDesign, memo)
+			memo[remainingDesign] = value
+			sum += value
+		}
+	}
+
 	return sum
 }
