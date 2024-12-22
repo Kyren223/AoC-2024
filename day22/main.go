cb6096efc0885a73773fa613593b41d41cc3c8e4
Kyren223 <Kyren223@proton.me>
2024-12-22 10:47:47 +0200

    Day 22 Part 1 Done

--- day22/main.go
+++ day22/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,13 +16,13 @@ func main() {
 	input := string(file)
 	fmt.Println("Part 1 Example:", Part1(input))
 
-	// file, err = os.ReadFile("input.txt")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// input = string(file)
-	// fmt.Println("Part 1:", Part1(input))
-	//
+	file, err = os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input = string(file)
+	fmt.Println("Part 1:", Part1(input))
+
 	// file, err = os.ReadFile("example.txt")
 	// if err != nil {
 	// 	log.Fatalln(err)
@@ -37,8 +39,41 @@ func main() {
 }
 
 func Part1(input string) int {
+	var secrets []int64
+	for _, line := range strings.Split(input, "\n") {
+		if line == "" {
+			continue
+		}
+
+		secret, err := strconv.ParseInt(line, 10, 64)
+		if err != nil {
+			panic("Huh")
+		}
+		secrets = append(secrets, secret)
+	}
+
 	sum := 0
+	for _, initialSecret := range secrets {
+		secret := initialSecret
+		for i := 0; i < 2000; i++ {
+			secret = Generate(secret)
+		}
+		fmt.Println(secret)
+		sum += int(secret)
+	}
+
 	return sum
+}
+
+func Generate(secret int64) int64 {
+	secret ^= secret<<6
+	secret = secret % 16777216 // 2^24
+	secret ^= secret>>5
+	secret = secret % 16777216 // 2^24
+	secret ^= secret<<11
+	secret = secret % 16777216 // 2^24
+
+	return secret
 }
 
 func Part2(input string) int {
